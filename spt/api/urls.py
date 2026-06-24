ef7ae2e99bcde03c8e5b53015f28264269d4f816
dodiwangkarabi-lang <dodiwangkarabi@gmail.com>
2026-06-25 07:50:57 +0800

    add: edit pegawai

--- spt/api/urls.py
+++ spt/api/urls.py
@@ -7,7 +7,7 @@ from . import views
 app_name = 'spt_api'
 
 # default router
-router = DefaultRouter()    
+# router = DefaultRouter()    
 
 # router.register(r'permohonan-sptq', PermohonanSPTViewSet, basename='permohonan-sptq')
 # router.register(r'sptq', SPTViewSet, basename='sptq')
@@ -16,8 +16,43 @@ urlpatterns = [
     # path("workflow/", include("spt.api.workflow_api.urls", namespace="workflow_api")),
     # path('spt/', include("spt.api.spt_api.urls", namespace="spt_api")),
     
+    path(
+        "permohonan-sptq/",
+        PermohonanSPTViewSet.as_view({
+            "get": "list",
+            "post": "create",
+        })
+    ),
     
-    # path('spt/<int:spt_id>/cetak/pdf/', views.CetakLaporanPelaksaanTugas.as_view(), name='cetak_pdf'),
+    
+    path('spt/<int:spt_id>/cetak/pdf/', views.CetakLaporanPelaksaanTugas.as_view(), name='cetak_pdf'),
 ]
 
-urlpatterns += router.urls
+# urlpatterns += router.urls
+
+
+
+
+
+
+
+
+
+
+
+
+# from rest_framework.routers import DefaultRouter
+
+# router = DefaultRouter()
+
+# print("router dibuat")
+
+# urlpatterns = []
+
+# print("sebelum urls")
+
+# x = router.urls
+
+# print("sesudah urls")
+
+# urlpatterns += x

8e4e6a4e7bb430dace58f9a7d1278d4645e7996c
dodiwangkarabi-lang <dodiwangkarabi@gmail.com>
2026-06-24 15:05:28 +0800

    add: dfdf

--- spt/permohonan/urls.py
+++ spt/permohonan/urls.py
@@ -4,6 +4,6 @@ app_name = "permohonan"
 
 urlpatterns = [
     path("htmx/", include("spt.permohonan.htmx.urls", namespace="permohonan_htmx")),
-    path("api/", include("spt.permohonan.api.urls", namespace="permohonan_api")),
+    path("apii/", include("spt.permohonan.api.urls", namespace="permohonan_api")),
     path("", include("spt.permohonan.web.urls", namespace="permohonan_web")),
 ]

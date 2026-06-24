17eb37d55620e401c0c94e7ddb3bf65fd39e6aa5
dodiwangkarabi-lang <dodiwangkarabi@gmail.com>
2026-06-24 14:52:49 +0800

    remove: drf

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,30 +1,30 @@
 from .views import UserViewSet, ProfileViewSet
-from rest_framework.routers import DefaultRouter
+# from rest_framework.routers import DefaultRouter
 
 from django.urls import path
 from . import views
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+#     TokenVerifyView
+# )
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='user')
-router.register(r'profiles', ProfileViewSet, basename='profile')
+# router = DefaultRouter()
+# router.register(r'users', UserViewSet, basename='user')
+# router.register(r'profiles', ProfileViewSet, basename='profile')
 
 urlpatterns = [
     path("user-aktif/", views.UserAktifView.as_view(), name="accounts_api_user_aktif"),
     path("user/", views.UserView.as_view(), name="accounts_api_user"),
     path("user/<int:user_id>/", views.UserDetailView.as_view(), name="accounts_api_user_detail"),
-    path("token/", TokenObtainPairView.as_view(), name="accounts_api_token"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="accounts_api_token_refresh"),
-    path("token/verify/", TokenVerifyView.as_view(), name="accounts_api_token_verify"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
-    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+    # path("token/", TokenObtainPairView.as_view(), name="accounts_api_token"),
+    # path("token/refresh/", TokenRefreshView.as_view(), name="accounts_api_token_refresh"),
+    # path("token/verify/", TokenVerifyView.as_view(), name="accounts_api_token_verify"),
+    # path("login/", TokenObtainPairView.as_view(), name="login"),
+    # path("refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("akuns/", views.AkunListView.as_view(), name="akuns"),
     path("akuns/me/", views.AkunMeView.as_view(), name="akuns_me"),
 ]
 
-urlpatterns += router.urls
+# urlpatterns += router.urls
